d2b56a3a39c16f494a5568c00064b567b9d8ef03
BabaDee94 <adedayoadenle27@gmail.com>
2024-06-27 17:26:06 +0100

    first commit

---
+++ flexbox/main.js
@@ -0,0 +1,137 @@
+// const myHeading = document.querySelector("h1")
+// myHeading.textContent = "Hello World";
+
+// const myImage = document.querySelector("img");
+
+// myImage.onclick = () => {
+//   const mySrc = myImage.getAttribute("src");
+//   if (mySrc === "images/download.png-icon.png") {
+//     myImage.setAttribute("src", "images/download.png");
+//   } else {
+//     myImage.setAttribute("src", "images/download.png-icon.png");
+//   }
+
+  
+  // function setUserName() {
+  //   const myName = prompt("Please enter your name.");
+  //   localStorage.setItem("name", myName);
+  //   myHeading.textContent = `Mozilla is cool, ${myName}`;
+  // }
+
+  // if (!localStorage.getItem("name")) {
+  //   setUserName();
+  // } else {
+  //   const storedName = localStorage.getItem("name");
+  //   myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  // }
+
+  // myButton.onclick = () => {
+  //   setUserName();
+  // }
+
+
+
+
+// const buttonA = document.querySelector("#button_A");
+// const headingA = document.querySelector("#heading_A");
+
+// buttonA.onclick = () => {
+//   const name = prompt("What is your name?");
+//   alert(`Hello ${name}, nice to see you!`);
+//   headingA.textContent = `Welcome ${name}`;
+// };
+
+// const buttonB = document.querySelector("#button_B");
+// const headingB = document.querySelector("#heading_B");
+
+// buttonB.onclick = () => {
+//   alert(`Hello ${prompt("What is yout name")}, nice to see you!`);
+//   headingB.textContent = `Welcome ${prompt("What is your name?")}`;
+// };
+
+
+const myVar = "JavaScript is amazing"
+
+console.log(myVar)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
